a7a167a709ed9d028bf2cb74593dcbccd6f931f9
Pedro Barquín <pbarquin@uxland.cat>
2022-07-18 10:00:01 +0200

    progress(ex 4): progress

---
+++ exercisi-4.js
@@ -0,0 +1,45 @@
+// N1 Ex.1. Crea una funció que, en executar-la, escrigui una frase en un fitxer.
+
+const fs = require('fs');
+
+const writeToFile = (path, text) => {
+    fs.writeFile(`${path}`, text, (err) => {
+        if (err)throw err;
+    })
+}
+
+console.log(writeToFile('test.txt','test'));
+// Created Output.txt && test.txt as tests
+
+// N1 Ex.2. Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
+
+const readFile = (path) => {
+    fs.readFile(`${path}`, 'utf8', (err, data) => {
+        if (err) {
+            throw err
+        }
+        ;
+        console.log(data);
+    })
+}
+console.log(readFile('Output.txt'));
+// Reads "test"
+
+// N1 Ex.3. Crea una funció que comprimeixi el fitxer del nivell 1.
+// Step 1: npm install compressing
+const compressing = require('compressing');
+const compressFile = (path, finalPath) => {
+    compressing.zip.compressFile(`${path}`, `${finalPath}`, () => {
+    if (err) {
+        throw err
+    }
+    ;
+    console.log(data);
+    })
+}
+console.log(compressFile('Output.txt', 'destination.zip'));
+
+
+
+
+
